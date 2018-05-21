b2375cd65eeafa75cd90f4c945aeebc8b76ff218
Dylan Rossi <ddrossi93@gmail.com>
2018-05-20 22:36:28 -0500

    now grabs players names involved in the play

--- stats_grabber.py
+++ stats_grabber.py
@@ -17,22 +17,60 @@ hits = []
 
 # sort events into lists
 for play in plays:
-    d = {
-        'event': '',
-        'x': '',
-        'y': ''
-    }
+
     event = play['result']['event']
-    try:
+
+    if event == 'Shot':
+        d = {
+            'event': '',
+            'shooter': '',
+            'goalie': '',
+            'x': '',
+            'y': ''
+        }
+        shooter = play['players'][0]['player']['fullName']
+        goalie = play['players'][1]['player']['fullName']
         x = play['coordinates']['x']
         y = play['coordinates']['y']
-    except:
-        x = 'NA'
-        y = 'NA'
-
-    d['event'] = event
-    d['x'] = x
-    d['y'] = y
+        d['event'] = event
+        d['shooter'] = shooter
+        d['goalie'] = goalie
+        d['x'] = x
+        d['y'] = y
+    elif event == 'Goal':
+        d = {
+            'event': '',
+            'shooter': '',
+            'goalie': '',
+            'x': '',
+            'y': ''
+        }
+        for player in play['players']:
+            if player['playerType'] == 'Scorer':
+                shooter = player['player']['fullName']
+            elif player['playerType'] == 'Goalie':
+                goalie = player['player']['fullName']
+        x = play['coordinates']['x']
+        y = play['coordinates']['y']
+        d['event'] = event
+        d['shooter'] = shooter
+        d['goalie'] = goalie
+        d['x'] = x
+        d['y'] = y
+    elif event == 'Hit':
+        d = {
+            'event': '',
+            'hitter': '',
+            'x': '',
+            'y': ''
+        }
+        hitter = play['players'][0]['player']['fullName']
+        x = play['coordinates']['x']
+        y = play['coordinates']['y']
+        d['event'] = event
+        d['hitter'] = hitter
+        d['x'] = x
+        d['y'] = y
 
     if event == 'Shot':
         shots.append(d)
@@ -40,3 +78,23 @@ for play in plays:
         goals.append(d)
     elif event == 'Hit':
         hits.append(d)
+
+
+# connect to db
+# TODO: add unique ID to each entry
+conn = 'mongodb://localhost:27017'
+client = pymongo.MongoClient(conn)
+
+db = client.nhlDB
+
+# add shot coordinates to db
+# TODO: add more info (player, team, period, etc.)
+db.shots.insert_many(shots)
+
+# add goal coordinates to db
+# TODO: add more info (player, team, period, etc.)
+db.goals.insert_many(goals)
+
+# add hit coordinates to db
+# TODO: add more info (player, team, period, etc.)
+db.hits.insert_many(hits)
