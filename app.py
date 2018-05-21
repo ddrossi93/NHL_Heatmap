ba7b62011e1a8768e32450eef04d4ff33ddcc780
Dylan Rossi <ddrossi93@gmail.com>
2018-05-20 23:25:00 -0500

    Started flask work. Fixed db name to work with flask_pymongo

---
+++ app.py
@@ -0,0 +1,25 @@
+# import dependencies
+from flask import Flask, render_template, jsonify, redirect, current_app
+from flask_pymongo import PyMongo
+import stats_grabber
+
+# create instance of Flask
+app = Flask(__name__)
+
+mongo = PyMongo(app)
+
+stats_grabber.get_data()
+
+# create index route
+
+
+@app.route("/")
+def index():
+    shot_data = mongo.db.shots.find()
+    # goal_data
+    # hit_data
+    return render_template("index.html", shot_data=shot_data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
